5046ba9f9f7b25e09c1134044db46b85232e52ef
shivam chaurasiya <shivamsharad7610@gmail.com>
2024-11-05 21:23:45 +0530

    changes settings, staticfiles, requirements, vercel file

--- EMS/EMS/settings.py
+++ EMS/EMS/settings.py
@@ -26,12 +26,13 @@ SECRET_KEY = 'django-insecure-195&qxlct*1=v^y(9xdemruv-1p9#f2f*cjy*-4&p9ouj+18ik
 DEBUG = True
 
 # this ALLOWED_HOSTS use for when deploy django project in vercel.
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +122,11 @@ USE_TZ = True
 
 # STATIC_URL, STATIC_ROOT use for when django project deploy in vercel.
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_DIRS = [os.path.join(BASE_DIR, 'admin_enquiry/static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR, 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
