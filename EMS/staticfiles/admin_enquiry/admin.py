1bd67928d4a740685dff4323b0ff4ece1fb07daa
shivam chaurasiya <shivamsharad7610@gmail.com>
2024-11-05 20:49:03 +0530

    add static file in user_enquiry and remove staticfile

--- EMS/staticfiles/admin_enquiry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AdminSignUp
-
-@admin.register(AdminSignUp)
-class AdminDataBaseAdmin(admin.ModelAdmin):
-    list_display=['id', 'name', 'email', 'password', 'confirm_password']
